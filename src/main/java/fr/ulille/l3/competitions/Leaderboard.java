4b673d505454d990b5ca027068664549f5c59587
Chagul <aurelien.plancke@gmail.com>
2021-09-29 16:06:06 +0200

    séparation tri map de leaderboard

--- src/main/java/fr/ulille/l3/competitions/Leaderboard.java
+++ src/main/java/fr/ulille/l3/competitions/Leaderboard.java
@@ -1,14 +1,11 @@
 package main.java.fr.ulille.l3.competitions;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import main.java.fr.ulille.l3.excpetions.EmptyCompetitorsListException;
+import main.java.fr.ulille.l3.util.MapUtil;
 
 public class Leaderboard {
 
@@ -37,36 +34,19 @@ public class Leaderboard {
 	 * @param aCompetitor
 	 */
 	public void incrScore(Competitor aCompetitor) {
-		this.ranking.replace(aCompetitor, this.ranking.get(aCompetitor) + 1);
+		this.ranking.put(aCompetitor, this.ranking.get(aCompetitor) + 1);
 	}
 
 	/**
 	 * 
 	 */
 	public void showRanking() {
-		sortRankingByScore();
+		ranking = MapUtil.sortByDescendingValue(ranking);
 		for (Map.Entry<Competitor,Integer> entryMap : ranking.entrySet()) {
-			System.out.println(entryMap.getKey().getName() + "-" + entryMap.getValue());
+			System.out.println(entryMap.getKey().getName() + "->" + entryMap.getValue());
 		}
 	}
 
-	/**
-	 * 
-	 */
-	private void sortRankingByScore() {
-		List<Map.Entry<Competitor, Integer>> listToBeSorted = new ArrayList<Map.Entry<Competitor, Integer>>(ranking.entrySet());
-		Collections.sort(listToBeSorted, new Comparator<Map.Entry<Competitor, Integer>>() {
-
-			@Override
-			public int compare(Entry<Competitor, Integer> o1, Entry<Competitor, Integer> o2) {
-				return o2.getValue() - o1.getValue();
-			}
-		});
-		ranking = new HashMap<>(listToBeSorted.size());
-		for(int indexList = 0; indexList < listToBeSorted.size(); indexList++) {
-			ranking.put(listToBeSorted.get(indexList).getKey(),listToBeSorted.get(indexList).getValue());
-		}
-	}
 
 	/**
 	 * 
