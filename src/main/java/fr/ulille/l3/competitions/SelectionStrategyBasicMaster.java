614b05ca406067de2965f340dee3a4fc39747cfc
Plancke Aurelien <plancke@a11p11.fil.univ-lille1.fr>
2021-10-19 09:57:20 +0200

    oubli fichier

--- src/main/java/fr/ulille/l3/competitions/SelectionStrategyBasicMaster.java
+++ src/main/java/fr/ulille/l3/competitions/SelectionStrategyBasicMaster.java
@@ -1,5 +1,26 @@
 package main.java.fr.ulille.l3.competitions;
 
-public class SelectionStrategyBasicMaster {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import main.java.fr.ulille.l3.modele.Competitor;
+import main.java.fr.ulille.l3.util.MapUtil;
+
+public class SelectionStrategyBasicMaster implements SelectionStrategy{
+
+	private final int NUMBER_OF_COMPETITOR_SELECTED = 2;
+	@Override
+	public List<Competitor> selection(List<League> groupPhase) {
+		List<Competitor> selectedCompetitors  = new ArrayList<>();
+		for(League l : groupPhase) {
+			Map<Competitor,Integer> groupRank = l.getLeaderboard().getRanking();
+			MapUtil.sortByDescendingValue(groupRank);
+			Set<Map.Entry<Competitor,Integer>> entryMap = groupRank.entrySet();
+			
+		}
+		return selectedCompetitors;
+	}
 
 }
